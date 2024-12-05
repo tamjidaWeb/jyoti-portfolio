c58ed77ad56e206c30e485a8df151ecbc174bc33
Abdullah Al Maruf <marufoffice.67@gmail.com>
2024-12-05 15:14:22 +0600

    commited

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+      document.querySelector(".style-switcher").classList.toggle("open");
+})
+window.addEventListener("scroll", () => {
+      if (document.querySelector(".style-switcher").classList.contains("open")) {
+            document.querySelector("style-switcher").classList.removes("open");
+      }
+})
+
+console.log('connected');
