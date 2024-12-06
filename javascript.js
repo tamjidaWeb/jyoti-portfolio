ff2b6d9482a2c8f2355c9b727d944cd524a54d5a
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2024-12-06 10:50:25 +0600

    commit js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,37 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
       document.querySelector(".style-switcher").classList.toggle("open");
 })
+
+// toggle style switcher
+
 window.addEventListener("scroll", () => {
       if (document.querySelector(".style-switcher").classList.contains("open")) {
-            document.querySelector("style-switcher").classList.removes("open");
+            document.querySelector(".style-switcher").classList.remove("open");
       }
 })
 
-console.log('connected');
+//  theme colors
+const alternateStyles = document.querySelector(".alternare-style");
+function setActiveStyle(color){
+      alternateStyles.forEach(style) => {
+            if(color === style.getAttribute('title'))
+                  {
+                  style.removeAttribute("disabled", "true");
+            }
+      })    
+}   
+
+
+//  theme light and dark mode 
+
+const dayNight = document.querySelector(".day-night");
+window.addEventListener("load", ()=>{
+      if(document.body.classList.contains("dark"))
+      {
+            dayNight.querySelector('i').classList.add("fa-sun");
+
+      }
+      else{
+            dayNight.querySelector('i').classList.add("fa-moon");
+      }
+})
