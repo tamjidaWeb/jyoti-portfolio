3ab0fd11f656473c0c7a2d4f8ce1b2f3f4d0ba8e
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2024-12-07 12:10:58 +0600

    dark mode activated

--- javascript.js
+++ javascript.js
@@ -12,20 +12,29 @@ window.addEventListener("scroll", () => {
 })
 
 //  theme colors
-const alternateStyles = document.querySelector(".alternare-style");
-function setActiveStyle(color){
-      alternateStyles.forEach(style) => {
-            if(color === style.getAttribute('title'))
-                  {
-                  style.removeAttribute("disabled", "true");
-            }
-      })    
-}   
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyles.forEach(style => {
+        if (color === style.getAttribute('title')) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+  
 
 
 //  theme light and dark mode 
 
 const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener('click', ()=> {
+      dayNight.querySelector('i').classList.toggle('fa-sun');
+      dayNight.querySelector('i').classList.toggle('fa-moon');
+      document.body.classList.toggle('dark');
+      
+
+})
 window.addEventListener("load", ()=>{
       if(document.body.classList.contains("dark"))
       {
