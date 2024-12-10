c8c84a257ac057f912fe9f60dfb8ee1ffa4f5358
Abdullah Al Maruf <marufoffice.67@gmail.com>
2024-12-10 11:38:51 +0600

    all problem solved

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,52 +1,43 @@
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
-      document.querySelector(".style-switcher").classList.toggle("open");
-})
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
 
 // toggle style switcher
 
 window.addEventListener("scroll", () => {
-      if (document.querySelector(".style-switcher").classList.contains("open")) {
-            document.querySelector(".style-switcher").classList.remove("open");
-      }
-})
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
 
 //  theme colors
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(colors) {
-    alternateStyles.forEach(style => {
-        if (colors === style.getAttribute('title')) {
+    alternateStyles.forEach((style) => {
+        if (colors === style.getAttribute("title")) {
             style.removeAttribute("disabled");
         } else {
             style.setAttribute("disabled", "true");
         }
     });
 }
-  
 
-
-//  theme light and dark mode 
+//  theme light and dark mode
 
 const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener('click', ()=> {
-      dayNight.querySelector('i').classList.toggle('fa-sun');
-      dayNight.querySelector('i').classList.toggle('fa-moon');
-      document.body.classList.toggle('dark');
-      
-
-})
-window.addEventListener("load", ()=>{
-      if(document.body.classList.contains("dark"))
-      {
-            dayNight.querySelector('i').classList.add("fa-sun");
-
-      }
-      else{
-            dayNight.querySelector('i').classList.add("fa-moon");
-      }
-})
-
-
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
 
 // document.getElementById("downloadCvButton").addEventListener("click", () => {
 //       const fileUrl = "resume.pdf"; // Replace with your CV file path
@@ -56,4 +47,3 @@ window.addEventListener("load", ()=>{
 //       anchor.click(); // Trigger the download
 //       anchor.remove(); // Clean up the DOM
 //   });
-  
