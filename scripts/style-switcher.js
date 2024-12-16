f2319586a95fb20f934a634b9ea3b8781257359e
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2024-12-17 00:48:30 +0600

    commit

--- scripts/style-switcher.js
+++
@@ -1,6 +0,0 @@
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
-      document.querySelector(".style-switcher").classList.toggle("open");
-})
-
-console.log('connected');
